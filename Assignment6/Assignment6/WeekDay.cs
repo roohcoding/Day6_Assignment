fe8ae10d0812c472287a5c1668d91eb7b996e758
Rupali Natewad <rupalirnatewad@gmail.com>
2022-05-10 15:21:40 +0530

    uploading WeekDay

---
+++ Assignment6/Assignment6/WeekDay.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment6
+{
+    internal class WeekDay
+    {
+        public static void dayofWeek(int D, int M, int Y)
+        {
+            int d1, m1, y1, x;
+
+
+            y1 = Y - (14 - M) / 12;
+            x = y1 + (y1 / 4) - y1 / 100 + (y1 / 400);
+            m1 = M + 12 * (14 - M) / 12 - 2;
+            d1 = (D + x + 31 * m1 / 12) % 7;
+
+            Console.WriteLine(d1);
+            switch (d1)
+            {
+                case 0:
+                    Console.WriteLine("SUNDAY");
+                    break;
+                case 1:
+                    Console.WriteLine("MONDAY");
+                    break;
+                case 2:
+                    Console.WriteLine("TUESDAY");
+                    break;
+                case 3:
+                    Console.WriteLine("WEDNESDAY");
+                    break;
+                case 4:
+                    Console.WriteLine("THURSDAY");
+                    break;
+                case 5:
+                    Console.WriteLine("FRIDAY");
+                    break;
+                case 6:
+                    Console.WriteLine("SATURDAY");
+                    break;
+
+            }
+        }
+    }
+}
