cc0c8044144469030752b3d4fd23a4810bed8fb9
Rupali Natewad <rupalirnatewad@gmail.com>
2022-05-10 13:59:50 +0530

    uploading Febonacci

--- Assignment6/Assignment6/Binary.cs
+++ Assignment6/Assignment6/Binary.cs
@@ -8,5 +8,24 @@ namespace Assignment6
 {
     internal class Binary
     {
+        public static void Binarycon()
+
+        {
+            Console.Write("Enter the Decimal Number : ");
+            int number = int.Parse(Console.ReadLine());
+            int i;
+            int[] numberArray = new int[10];
+            for (i = 0; number > 0; i++)
+            {
+                numberArray[i] = number % 2;
+                number = number / 2;
+            }
+            Console.Write("Binary Represenation of the given Number : ");
+            for (i = i - 1; i >= 0; i--)
+            {
+                Console.Write(numberArray[i]);
+            }
+            Console.ReadKey();
+        }
     }
 }
--- Assignment6/Assignment6/Program.cs
+++ Assignment6/Assignment6/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Assignment6;
+
 Console.WriteLine("Hello, World!");
+Binary.Binarycon();
